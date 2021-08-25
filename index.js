0dcc343fa3db0715ad28475ec6f17eb05ad5b26d
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2021-08-25 13:25:47 -0400

    visual tweaks

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
-var itemList = ["boxes", "envelopes", "poly-mailers"];
+var itemList = ["Boxes", "Envelopes", "Poly-Mailers"];
 var cityList= ["toronto", "vancouver", "ottawa"];
 var currentItem = 0;
+var currentItem2 = 0;
 
 var item = document.getElementById("item");
 
@@ -15,3 +16,16 @@ setInterval(function() {
     
 }, 3000);
 
+/*
+var location = document.getElementById("location");
+
+setInterval(function() {
+        location.innerHTML = cityList[currentItem2];
+        currentItem2++;
+    
+        if(currentItem2 === cityList.length){ 
+        currentItem2 = 0;
+        }
+    
+}, 4000);
+*/
