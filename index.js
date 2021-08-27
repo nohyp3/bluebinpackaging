57160f50281c61adfddc347d92ca475ef2dc74fb
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2021-08-27 12:10:27 -0400

    minor visual tweaks

--- index.js
+++ index.js
@@ -16,16 +16,4 @@ setInterval(function() {
     
 }, 3000);
 
-/*
-var location = document.getElementById("location");
 
-setInterval(function() {
-        location.innerHTML = cityList[currentItem2];
-        currentItem2++;
-    
-        if(currentItem2 === cityList.length){ 
-        currentItem2 = 0;
-        }
-    
-}, 4000);
-*/
