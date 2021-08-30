f47e8aee86a5685e3f117209d97fc06ea75dc440
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2021-08-30 12:06:10 -0400

    primitive search bar functionality

--- canirecycle.js
+++ canirecycle.js
@@ -1,21 +1,52 @@
 function search() {
-    var input, filter, i, txtValue, box;
+    var input, filter, box, envelope, mailer, bubble;
     input = document.getElementById('search-bar');
     filter = input.value.toUpperCase();
     box = document.getElementById("box");
-    var text = document.getElementById("search-text")
-  /*  for (i = 0; i < li.length; i++) {
-        var items = ["box"]
-        txtValue = items[i];
-        if (true) {
-          box.style.display = "";
-        } else {
-          box.style.display = "none";
-        }
-      }
+    envelope = document.getElementById("envelope");
+    mailer = document.getElementById("mailer");
+    bubble = document.getElementById("bubble");
+    var text = document.getElementById("search-text");
+    var items = ["BOX", "KRAFT ENVELOPE", "POLYMAILER", "BUBBLE WRAP", ""];
+    if(filter == items[0]) {
+      box.style.display = "";
+      envelope.style.display = "none";
+      mailer.style.display = "none";
+      bubble.style.display = "none";
+      text.innerText = "";
     }
-    */
-   text.innerText = input.value + " lol i still don't work";
-}
+    else if(filter == items[1]) {
+      box.style.display = "none";
+      envelope.style.display = "";
+      mailer.style.display = "none";
+      bubble.style.display = "none";
+      text.innerText = "";
+    }
+    else if(filter == items[2]) {
+      box.style.display = "none";
+      envelope.style.display = "none";
+      mailer.style.display = "";
+      bubble.style.display = "none";
+      text.innerText = "";
+    }
+    else if(filter == items[3]) {
+      box.style.display = "none";
+      envelope.style.display = "none";
+      mailer.style.display = "none";
+      bubble.style.display = "";
+      text.innerText = "";
+    }
+    else if(filter == items[4]) {
+      box.style.display = "";
+      envelope.style.display = "";
+      mailer.style.display = "";
+      bubble.style.display = "";
+      text.innerText = "";
+    }
+    else {
+      text.innerText = "not found";
+    }
+  }
+  
 
 
