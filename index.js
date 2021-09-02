a9d3cbcef19a431310169f41f238a85edd8a71f9
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2021-09-02 11:35:49 -0400

    added images to interval function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var itemList = ["Boxes", "Envelopes", "Poly-Mailers"];
 var cityList= ["toronto", "vancouver", "ottawa"];
+var imageList = ["/images/box.svg", "/images/kraftenvelope.svg", "/images/polymailer.svg"];
 var currentItem = 0;
 var currentItem2 = 0;
 
@@ -8,6 +9,8 @@ var item = document.getElementById("item");
 
 setInterval(function() {
         item.innerHTML = itemList[currentItem];
+        document.getElementById("hero-image").src = imageList[currentItem];
+
         currentItem++;
     
         if(currentItem === itemList.length){ 
