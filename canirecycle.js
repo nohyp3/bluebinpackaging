154c6d42e2944fb9e3f9d86d8b67044f5fe1f562
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2021-09-10 19:31:35 -0400

    minor tweaks

--- canirecycle.js
+++ canirecycle.js
@@ -1,5 +1,6 @@
+var searchBar = document.getElementById('search-bar');
 function search_grid() {
-    let input = document.getElementById('search-bar').value
+    let input = document.getElementById('search-bar').value;
     input=input.toLowerCase();
     let x = document.getElementsByClassName('grid-item');
       
@@ -8,9 +9,25 @@ function search_grid() {
             x[i].style.display="none";
         }
         else {
-            x[i].style.display="";                 
+            x[i].style.display="";
         }
     }
 }
+//menu button
+//menu buttom
+var menu = document.getElementById('menu');
+var links = document.getElementById('mobile-links');
+var menuOnOff = 0;
+menu.addEventListener('click', () => {
+    menu.style.display = "none";
+    if( menuOnOff == 0) {
+        links.style.setProperty('display', 'flex');
+        menuOnOff = 1;
+    }
+    else {
+        links.style.setProperty('display', 'none');
+        menuOnOff = 0;
+    }
+})
 
 
--- index.js
+++ index.js
@@ -17,4 +17,18 @@ setInterval(function() {
     
 }, 3000);
 
-
+//menu buttom
+var menu = document.getElementById('menu');
+var links = document.getElementById('mobile-links');
+var menuOnOff = 0;
+menu.addEventListener('click', () => {
+    menu.style.display = "none";
+    if( menuOnOff == 0) {
+        links.style.setProperty('display', 'flex');
+        menuOnOff = 1;
+    }
+    else {
+        links.style.setProperty('display', 'none');
+        menuOnOff = 0;
+    }
+})
